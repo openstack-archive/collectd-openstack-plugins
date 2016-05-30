3b2027c9f6f935bbbede1af56bef4a5a02bb53b8
Emma Foley <emma.l.foley@intel.com>
2016-05-30 16:05:08 +0100

    Add counter type mappings
    Add mappings between counter type in collectd and Ceilometer.
    Previously, all counters were given "gauge" type.
    Now, the following mappings are applied:
        - gauge -> gauge
        - derive -> delta
        - absolute, counter -> cumulative
    
    Closes-bug #1583301
    
    Change-Id: I13e75fcc57ed2b44c2b9a02ee9562f70748d01c2

--- collectd_ceilometer/meters/base.py
+++ collectd_ceilometer/meters/base.py
@@ -15,7 +15,13 @@
 
 from __future__ import unicode_literals
 
+# pylint: disable=import-error
+import collectd
+# pylint: enable=import-error
 from collectd_ceilometer.settings import Config
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 class Meter(object):
@@ -42,3 +48,21 @@ class Meter(object):
         """Get meter unit"""
         # pylint: disable=no-self-use
         return Config.instance().unit(vl.plugin, vl.type)
+
+    def sample_type(self, vl):
+        """Translate from collectd counter type to Ceilometer type"""
+        types = {"gauge": "gauge",
+                 "derive": "delta",
+                 "absolute": "cumulative",
+                 "counter": "cumulative"}
+
+        try:
+            # get_dataset -> [('value', 'derive', 0.0, None)]
+            collectd_type = collectd.get_dataset(str(vl.type))[0][1]
+        except Exception:
+            LOGGER.warning(
+                "Cannot map counter type '%s': using type 'gauge'.", vl.type,
+                exc_info=1)
+            collectd_type = "gauge"
+
+        return types[collectd_type]
