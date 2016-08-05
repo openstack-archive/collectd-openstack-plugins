b68d5bbe5cf181bfba2ab34fe66a98cf5bcd3fcf
Federico Ressi <federico.ressi@intel.com>
2016-08-05 18:40:32 +0100

    Refactor and test log handler
    - Write unit tests for the handler
    - Forward constructor parameters to the super-class
    - Takes verbosity from outside instead of using configuration class
    
    Closes-Bug: #1610348
    Change-Id: I15b96d14e513889efbb50e2c51863bc224e71465

---
+++ collectd_ceilometer/tests/mocking.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import inspect
+
+import mock
+
+
+def full_class_name(cls):
+    """Returns the full qualified name of given class."""
+    assert isinstance(cls, type)
+    return inspect.getmodule(cls).__name__ + '.' + cls.__name__
+
+
+def patch_class(cls, autospec=True, *args, **kwargs):
+    """Parches given class."""
+    return mock.patch(full_class_name(cls), autospec=autospec)
