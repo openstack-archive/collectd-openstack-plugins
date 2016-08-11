79a95a8951374f2d8c2676bd9b4cdc0b95aecfee
Federico Ressi <federico.ressi@intel.com>
2016-08-11 11:22:55 +0100

    Make collecd available to plugin components
    Collectd module could be required by plugin components
    but importing it from their modules makes unit testing
    harder because it would require running tests from
    inside collecd process.
    
    This changes make the module available by passing it
    to components constructors making testing and mocking
    easier.
    
    Related-Bug #1583301
    
    Change-Id: I6abfd330c8f3b5108e78543df7c803cd65a15655

--- collectd_ceilometer/plugin.py
+++ collectd_ceilometer/plugin.py
@@ -65,7 +65,7 @@ class Plugin(object):
         """Initialization callback"""
 
         collectd.info('Initializing the collectd OpenStack python plugin')
-        self._meters = MeterStorage()
+        self._meters = MeterStorage(collectd=collectd)
         self._writer = Writer(self._meters)
 
     def write(self, vl, data=None):
