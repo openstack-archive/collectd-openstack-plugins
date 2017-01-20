eb3069512afeeb363aaf633dfa1743785be28127
Emma Foley <emma.l.foley@intel.com>
2017-01-20 16:25:16 +0000

    Remove pbr-dependent code
    - Remove the pbr import in colelctd_ceiloemter/__init__.py
    - PBR is still needed for a pip_install, but was causing import
      errors in the plugin.
    
    Change-Id: I24c6349caba3fdd097a5218cc048aa696bd27550
    Closes-Bug: #1644254

--- collectd_ceilometer/__init__.py
+++ collectd_ceilometer/__init__.py
@@ -11,8 +11,4 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""Collectd Ceilometer plugin implementation"""
-import pbr.version
-
-__version__ = pbr.version.VersionInfo(
-    'collectd_ceilometer_plugin')
+"""Collectd Ceilometer plugin"""
