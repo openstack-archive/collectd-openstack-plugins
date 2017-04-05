0f5a7efea7117e40463cec9358681fff40cd4de0
Michal Ptacek <michalx.ptacek@intel.com>
2017-04-05 12:37:49 +0100

    enables experimental collectd ci job
    - this change will enable testing of collectd-ceilometer-plugin
      in the upstream ci
    
    Change-Id: Ie9e5fc1316e3a4a76945100d977313e8b3540d44
    Closes-Bug: #1675072

---
+++ devstack/gate/post_test_hook_bin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# verify that db contains collectd metrics
+# give it ~10 mins
+
+retry_count=1
+max_retry_count=20
+
+export PATH=/usr/sbin:$PATH
+source /opt/stack/new/devstack/openrc admin
+
+while [ "$retry_count" -le "$max_retry_count" ]; do
+    if [ $(gnocchi metric list | grep interface | wc -l) -eq 0 ] || [ $(ceilometer meter-list -l 1000 | grep interface | wc -l) -eq 0 ]; then
+         echo "Testing metric interface not yet visible in db $retry_count/$max_retry_count"
+    else
+          echo "Testing metric obtained from db"
+          exit 0
+    fi
+    let retry_count="$retry_count+1"
+    sleep 30
+done
+echo "Testing metric interface not visible in db"
+exit 1
+
