293d500519995cca06ce35ee6181808f6318edbb
Michal Ptacek <michalx.ptacek@intel.com>
2017-04-05 12:37:49 +0100

    enables experimental collectd ci job
    - this change will enable testing of collectd-ceilometer-plugin
      in the upstream ci
    
    Change-Id: Ie9e5fc1316e3a4a76945100d977313e8b3540d44
    Closes-Bug: #1675072

---
+++ devstack/gate/pre_test_hook_src.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo collectd-ceilometer-plugin pre-test-hook executed
+
+# WA for https://github.com/pypa/setuptools_scm/issues/164
+sudo pip install setuptools_scm
+
+#if [ -f /usr/bin/yum ]; then
+#    sudo yum -y install python-gnocchiclient
+#elif [ -f /usr/bin/apt-get ]; then
+#    sudo apt-get install -y python-gnocchiclient
+#else
+#    # w/o gnocchi client there is no sense in cont.
+#    return 1 
+#fi
