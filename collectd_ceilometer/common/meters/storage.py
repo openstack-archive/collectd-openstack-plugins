9343c814fc6031255eb0375b1f3e6131a2c35521
Helena McGough <helena.mcgough@intel.com>
2017-06-30 13:47:24 +0000

    Deprecated the libvirt meter
     - Libvirt meter is only enabled if configured
     - Added deployment code
     - Included a reno
     - Updated doc/source/usage.rst
     - Updated unit tests
    
    Change-Id: Ia4eb566e087ff341505896534296e7afd338f98b

--- collectd_ceilometer/common/meters/storage.py
+++ collectd_ceilometer/common/meters/storage.py
@@ -19,6 +19,7 @@ import six
 
 from collectd_ceilometer.common.meters.base import Meter
 from collectd_ceilometer.common.meters.libvirt import LibvirtMeter
+from collectd_ceilometer.common.settings import Config
 
 
 class MeterStorage(object):
@@ -34,8 +35,11 @@ class MeterStorage(object):
         self._default = Meter(collectd=collectd)
 
         # fill dict with specialized meters classes
-        self._meters = {key: meter_class(collectd=collectd)
-                        for key, meter_class in six.iteritems(self._classes)}
+        if Config.instance().libvirt_enabled() is True:
+            # Deprecated: Enabled manually
+            self._meters = \
+                {key: meter_class(collectd=collectd)
+                 for key, meter_class in six.iteritems(self._classes)}
 
     def get(self, plugin):
         """Get meter for the collectd plugin"""
--- collectd_ceilometer/common/settings.py
+++ collectd_ceilometer/common/settings.py
@@ -58,6 +58,7 @@ class Config(object):
         CfgParam('OS_PASSWORD', None, six.text_type),
         CfgParam('OS_TENANT_NAME', None, six.text_type),
         CfgParam('VERBOSE', False, bool),
+        CfgParam('LIBVIRT_METER_ENABLED', False, bool),
 
         CfgParam('LIBVIRT_CONN_URI', 'qemu:///system', six.text_type),
     ]
@@ -75,6 +76,7 @@ class Config(object):
         # dictionary for user-defined units
         self._user_units = {}
         self._units = UNITS.copy()
+        self._libvirt_meter = False
 
     def read(self, cfg):
         """Read the collectd configuration
@@ -105,6 +107,10 @@ class Config(object):
             if unit:
                 return unit
         return self._units.get(plugin, "None")
+
+    def libvirt_enabled(self):
+        """Check if the libvirt meter is enabled"""
+        return self._libvirt_meter
 
     def _read_node(self, node):
         """Read a configuration node
@@ -149,6 +155,8 @@ class Config(object):
                 val = '*****'
             LOGGER.info(
                 'Got configuration parameter: %s -> "%s"', key, val)
+            if key == 'LIBVIRT_METER_ENABLED':
+                self._libvirt_meter = val
         else:
             LOGGER.error('Unknown configuration parameter "%s"', key)
 
