1a3db89bc7dde91125ed8cf4ee0f226ce78bc2e9
Helena McGough <helena.mcgough@intel.com>
2017-06-30 13:47:24 +0000

    Deprecated the libvirt meter
     - Libvirt meter is only enabled if configured
     - Added deployment code
     - Included a reno
     - Updated doc/source/usage.rst
     - Updated unit tests
    
    Change-Id: Ia4eb566e087ff341505896534296e7afd338f98b

--- collectd_ceilometer/common/meters/storage.py
+++ collectd_ceilometer/common/meters/storage.py
@@ -21,6 +21,7 @@ import threading
 
 from collectd_ceilometer.common.meters.base import Meter
 from collectd_ceilometer.common.meters.libvirt import LibvirtMeter
+from collectd_ceilometer.common.settings import Config
 
 
 class MeterStorage(object):
@@ -36,8 +37,11 @@ class MeterStorage(object):
         self._default = Meter(collectd=collectd)
 
         # fill dict with specialized meters classes
-        self._meters = {key: meter_class(collectd=collectd)
-                        for key, meter_class in six.iteritems(self._classes)}
+        if Config.instance().libvirt_enabled() is True:
+            # Deprecated: Enabled manually
+            self._meters = \
+                {key: meter_class(collectd=collectd)
+                 for key, meter_class in six.iteritems(self._classes)}
 
     def get(self, plugin):
         """Get meter for the collectd plugin"""
