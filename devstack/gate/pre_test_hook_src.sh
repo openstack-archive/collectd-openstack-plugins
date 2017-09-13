ef1e2eddbb8564a19f32c3fa5566ad2516ada2ed
Emma Foley <emma.l.foley@intel.com>
2017-09-13 17:32:59 +0100

    Update experimental gate jobs to actually run tests
    Add logic to pre_test_hook_{src,bin}.sh to change behaviour
    of experimental jobs based on install_type.
    
    Depends-On: I0b0775ad111ba1b225d77c56c71a0da67a96ab57
    Change-Id: I234fc5bfc06bc04194c5690ac198a04e82966ee0

--- devstack/gate/pre_test_hook_src.sh
+++ devstack/gate/pre_test_hook_src.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo collectd-ceilometer-plugin pre-test-hook executed
+export DEVSTACK_LOCAL_CONFIG=$DEVSTACK_LOCAL_CONFIG'
+COLLECTD_INSTALL_TYPE=source'
 
+cd /opt/stack/new/collectd-ceilometer-plugin/devstack/gate/
+. ./pre_test_hook_bin.sh
+cd -
