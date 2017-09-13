dd33e7762976de828a7c918307325e2a28dcd7f4
Emma Foley <emma.l.foley@intel.com>
2017-09-13 17:32:59 +0100

    Update experimental gate jobs to actually run tests
    Add logic to pre_test_hook_{src,bin}.sh to change behaviour
    of experimental jobs based on install_type.
    
    Change-Id: I234fc5bfc06bc04194c5690ac198a04e82966ee0

--- devstack/gate/pre_test_hook_src.sh
+++ devstack/gate/pre_test_hook_src.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-echo collectd-ceilometer-plugin pre-test-hook executed
+export DEVSTACK_LOCAL_CONFIG=$DEVSTACK_LOCAL_CONFIG'
+COLLECTD_INSTALL_TYPE=source'
 
+if [[ `uname -a` =~ 'Ubuntu' ]]; then
+
+    sudo apt-get install -y python-gnocchiclient
+    # Update apache config to allow access to metricdb, otherwise, apache2 will block communication
+    cd /opt/stack/new/gnocchi/devstack
+    sed -i '/IfVersion>/a <Location \/v1\/metric>\n        Require all granted\n    <\/Location>' apache-ported-gnocchi.template
+    cd -
+else
+    # Using CentOS
+    # The default gnocchi client doesn't work, so we have to install a particular version for CentOS
+    # install python-gnocchiclient stable/2.6
+    TEMP_DIR=$PWD
+    cd /tmp
+    sudo  git clone https://github.com/openstack/python-gnocchiclient
+    cd python-gnocchiclient
+    sudo git checkout remotes/origin/stable/2.6
+    sudo python setup.py install
+    cd $TEMP_DIR
+
+    # bypass uwsgi proble on CentOS
+    export DEVSTACK_LOCAL_CONFIG=$DEVSTACK_LOCAL_CONFIG'
+    WSGI_MODE="mod_wsgi"'
+fi
