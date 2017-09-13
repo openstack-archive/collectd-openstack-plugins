e4dd5c56606530a6b9514f4b2840f54f26e879e0
Emma Foley <emma.l.foley@intel.com>
2017-09-13 17:32:59 +0100

    Update experimental gate jobs to actually run tests
    Add logic to pre_test_hook_{src,bin}.sh to change behaviour
    of experimental jobs based on install_type.
    
    Depends-On: I0b0775ad111ba1b225d77c56c71a0da67a96ab57
    Change-Id: I234fc5bfc06bc04194c5690ac198a04e82966ee0

--- devstack/gate/post_test_hook_bin.sh
+++ devstack/gate/post_test_hook_bin.sh
@@ -17,7 +17,9 @@ export PATH=/usr/sbin:$PATH
 source /opt/stack/new/devstack/openrc admin
 
 while [ "$retry_count" -le "$max_retry_count" ]; do
-    if [ $(openstack metric metric list | grep interface | wc -l) -eq 0 ]; then
+    # TODO: remove this debug
+    openstack metric metric list
+    if [ $(openstack metric metric list | wc -l) -gt 4 ]; then
          echo "Testing metric interface not yet visible in db $retry_count/$max_retry_count"
     else
           echo "Testing metric obtained from db"
